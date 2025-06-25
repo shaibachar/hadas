baae46a5287f43ff1294bde18961435643e07cc9
shaibachar <shaibachar@gmail.com>
2025-06-25 12:04:43 +0300

    cleanup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 function App() {
   const [mode, setMode] = useState("link"); // 'link', 'pdf', 'text'
