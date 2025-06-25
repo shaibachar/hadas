4f5de06da45eec42f648a19795b27ff4749c7fc2
shaibachar <shaibachar@gmail.com>
2025-06-25 10:51:08 +0300

    fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:8000";
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
 
 function App() {
   const [link, setLink] = useState("");
